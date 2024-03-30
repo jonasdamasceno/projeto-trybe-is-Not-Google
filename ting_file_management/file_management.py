a8768e6e7e6e09dae032797598546808380d3e00
jdamasceno <jonasmichael2021@outlook.com>
2024-03-30 17:57:48 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, encoding="utf-8") as file:
+            lines = [line.rstrip() for line in file.readlines()]
+        return lines
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
