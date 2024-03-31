f9d978ca3b65344914a88e24ec605a229aa89d6d
jdamasceno <jonasmichael2021@outlook.com>
2024-03-30 23:41:44 -0300

    requisitos 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for item in instance._data:
+        if any(
+            word.lower() in line.lower() for line in item["linhas_do_arquivo"]
+        ):
+            occurrences = [
+                {"linha": i + 1}
+                for i, line in enumerate(item["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ]
+            result_item = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+            results.append(result_item)
+
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for item in instance._data:
+        if any(
+            word.lower() in line.lower() for line in item["linhas_do_arquivo"]
+        ):
+            occurrences = [
+                {"linha": i + 1, "conteudo": line}
+                for i, line in enumerate(item["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ]
+            result_item = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+            results.append(result_item)
+
+    return results
