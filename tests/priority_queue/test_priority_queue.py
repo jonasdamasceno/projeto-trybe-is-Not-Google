739a3ebf811b4e583a10e17baba5350951ba5e8b
jdamasceno <jonasmichael2021@outlook.com>
2024-03-30 23:31:26 -0300

    requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue({"qtd_linhas": 3})
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 6})
+    assert len(priority_queue) == 3
+    assert len(priority_queue.high_priority) == 2
+    assert len(priority_queue.regular_priority) == 1
+
+    priority_queue.dequeue()
+    assert len(priority_queue) == 2
+
+    teste = priority_queue.search(0)
+    assert teste["qtd_linhas"] == 4
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(5)
