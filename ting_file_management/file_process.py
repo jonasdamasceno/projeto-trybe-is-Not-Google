2b5dc8924b1003067d3ab6f97bd2a95aa29c4d4b
jdamasceno <jonasmichael2021@outlook.com>
2024-03-30 22:46:26 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines = txt_importer(path_file)
+    if any(item["nome_do_arquivo"] == path_file for item in instance._data):
+        return False
+    obj_enqueue = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines,
+    }
+    instance.enqueue(obj_enqueue)
+
+    return sys.stdout.write(str(obj_enqueue))
 
 
 def remove(instance):
@@ -8,3 +22,25 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# new_queue = Queue()
+# new_queue.enqueue(
+#     {
+#         "nome_do_arquivo": "teste.txt",
+#         "qtd_linhas": 4,
+#         "linhas_do_arquivo": ["Teste", "teste"],
+#     }
+# )
+# new_queue.enqueue(
+#     {
+#         "nome_do_arquivo": "arquivo_teste.txt",
+#         "qtd_linhas": 3,
+#         "linhas_do_arquivo": ["Teste", "teste"],
+#     }
+# )
+
+# # print(new_queue.search(1))
+# print(process("statics/arquivo_teste.txt", new_queue))
+# process("statics/nome_pedr.txt", new_queue)
+# print(new_queue.data)
